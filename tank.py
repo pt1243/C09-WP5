d06c56a3c38fee36f5ab087e7b022c1f3ee964bd
Sanyigazda <105061782+Sanyigazda@users.noreply.github.com>
2022-12-07 15:26:58 +0100

    Update tank.py

--- tank.py
+++ tank.py
@@ -11,11 +11,18 @@ R = 0.5
 p = 10e5
 hw = 0.2
 
+p_space = 1e2
+
 sigma_euler = (np.pi**2 * E * I) / (A * L**2)
 
 Q = (p/E)*(R/t1)**2
 k = hw + (12/np.pi**4)*(L**4/R**2 * t1**2)*(1-v**2)*(1/hw)
 sigma_shell = (1.983 - 0.983*np.exp(-23.14*Q))*k*((np.pi**2 * E)/(12*(1-v**2)))*(t1/L)**2
 
+sigma_circ = ((p-p_space)*R)/t1
+sigma_long = ((p-p_space)*R)/(2*t1)
+
 print("Eluer critical load (MPa):", sigma_euler/10e6)
 print("\nShell critical load (MPa):", sigma_shell/10e6)
+print("\nCircumferential stress (MPa):", sigma_circ/10e6)
+print("\nLongitudinal stress (MPa):", sigma_long/10e6)
