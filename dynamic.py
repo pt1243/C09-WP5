24eced64c26a86fc014ed5569ad8a009046e5c78
tzoli6 <zozoturi@gmail.com>
2022-12-19 16:45:20 +0100

    Update dynamic.py

--- dynamic.py
+++ dynamic.py
@@ -1,5 +1,6 @@
 from definitions import FuelTank
 import numpy as np
+import matplotlib.pyplot as plt
 
 # S/C Adapter Dimensions and Material
 d_1 = 1666*10**-3
@@ -19,3 +20,25 @@ print('Spacecraft adapter stiffness: ' +str(K*10**-6)+'kN/mm')
 M = 1350 # kg S/C mass REVISE IT
 f_sc = 1/(2*np.pi)*np.sqrt(K/M)
 print('Spacecraft Natural Frequency: '+ str(f_sc))
+
+#solution
+K = 50000 #N/m
+M = 100 #kg
+g = 9.81 #m/s^2
+w_f = 10 #
+w_n = np.sqrt(K/M)
+f_0 = 0.8*g/M
+x_0 = 0
+v_0 = 10
+
+def x_h(t):
+    return (x_0-f_0/(w_n**2+w_f**2))*np.cos(w_n*t)+v_0/w_n*np.sin(w_n*t)
+
+def x_p(t):
+    return (f_0/(w_n**2-w_f**2))*np.cos(w_f*t)
+
+t = np.linspace(0,1,200)
+x = x_h(t)+x_p(t)
+print(x)
+plt.plot(t, x)
+plt.show()
